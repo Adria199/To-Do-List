f122efc292468fed390137d6213844f2789a85db
Adrian Guilherme <adrianguilherme133@outlook.com>
2023-02-27 16:45:19 -0300

    finished searchBar component display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.module.css'
 import { Header } from './components/Header'
+import { SearchBar } from './components/SearchBar'
+
+import styles from './App.module.css'
 
 function App() {
 
 
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <Header />
+       <section>
+         <SearchBar />
+       </section>
     </div>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,15 @@
 import styles from './SearchBar.module.css'
 
+import Plus from '../assets/plus.svg'
+
 export function SearchBar() {
     return (
-        <div></div>
+        <div className={styles.searchContainer}>
+            <input className={styles.searchBar} type="text" placeholder='Add a new task'/>
+            <button 
+                 className={styles.createBtn}>
+                 Create <img src={Plus}/> 
+            </button>
+        </div>
     )
 }
